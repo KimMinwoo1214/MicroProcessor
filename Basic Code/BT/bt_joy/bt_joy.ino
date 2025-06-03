47cafe07cb86d144aa6141ae736f69e024336943
KimMinwoo1214 <werkm1214@hanyang.ac.kr>
2025-06-03 17:32:51 +0900

    BT_joy

--- Basic Code/BT/bt_joy/bt_joy.ino
+++ Basic Code/BT/bt_joy/bt_joy.ino
@@ -14,7 +14,10 @@ SoftwareSerial mySerial(13, 12); // TX=13, RX=12 for Bluetooth module
 
 void init_ADC() {
   ADMUX = (1 << REFS0); // AVcc reference
-  ADCSRA = (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0); // ADC enable, Prescaler 128
+  ADCSRA = (1 << ADEN)
+         | (1 << ADPS2)
+         | (1 << ADPS1)
+         | (1 << ADPS0); // ADC enable, Prescaler 128
 }
 
 int read_ADC(uint8_t channel) {
@@ -28,39 +31,48 @@ int read_ADC(uint8_t channel) {
 struct DataPacket {
   int16_t X;
   int16_t Y;
-  char buttons[5];
+  char    buttons[5];
 };
 
 void setup() {
   Serial.begin(9600);
-  mySerial.begin(38400);
+  mySerial.begin(9600); // SoftwareSerial 속도를 9600으로 낮춤
   init_ADC();
 
   DDRD &= ~(PORTD_BUTTON_A | PORTD_BUTTON_B | PORTD_BUTTON_C | PORTD_BUTTON_D);
-  PORTD |= (PORTD_BUTTON_A | PORTD_BUTTON_B | PORTD_BUTTON_C | PORTD_BUTTON_D);
+  PORTD |=  (PORTD_BUTTON_A | PORTD_BUTTON_B | PORTD_BUTTON_C | PORTD_BUTTON_D);
   DDRB &= ~PORTB_BUTTON_E;
-  PORTB |= PORTB_BUTTON_E;
+  PORTB |=  PORTB_BUTTON_E;
 }
 
 void loop() {
-  int X = read_ADC(X_CHANNEL);
-  int Y = read_ADC(Y_CHANNEL);
+  int rawX = read_ADC(X_CHANNEL);
+  int rawY = read_ADC(Y_CHANNEL);
 
-  char buttons[5] = {'0', '0', '0', '0', '0'};
-  if (!(PIND & PORTD_BUTTON_A)) buttons[0] = 'A';
-  if (!(PIND & PORTD_BUTTON_B)) buttons[1] = 'B';
-  if (!(PIND & PORTD_BUTTON_C)) buttons[2] = 'C';
-  if (!(PIND & PORTD_BUTTON_D)) buttons[3] = 'D';
-  if (!(PINB & PORTB_BUTTON_E)) buttons[4] = 'E';
+  // 버튼 상태를 저장할 때, 마지막에 '\0'을 넣기 위해 6바이트 배열로 선언 후 복사
+  char tempBtn[6] = {'0','0','0','0','0','\0'};  
+  if (!(PIND & PORTD_BUTTON_A)) tempBtn[0] = 'A';
+  if (!(PIND & PORTD_BUTTON_B)) tempBtn[1] = 'B';
+  if (!(PIND & PORTD_BUTTON_C)) tempBtn[2] = 'C';
+  if (!(PIND & PORTD_BUTTON_D)) tempBtn[3] = 'D';
+  if (!(PINB & PORTB_BUTTON_E)) tempBtn[4] = 'E';
 
+  // DataPacket에 채워 넣을 때는 버튼 5개만, 널 종료문자는 구조체에 포함하지 않음
+  DataPacket dataPacket;
+  dataPacket.X = (int16_t)rawX;
+  dataPacket.Y = (int16_t)rawY;
+  for (int i = 0; i < 5; i++) {
+    dataPacket.buttons[i] = tempBtn[i];
+  }
 
-  DataPacket dataPacket = { X, Y, {buttons[0], buttons[1], buttons[2], buttons[3], buttons[4]} };
-
+  // 패킷 전송 (총 2+2+5 = 9바이트)
   mySerial.write((uint8_t*)&dataPacket, sizeof(dataPacket));
 
-  Serial.print("X: "); Serial.print(X);
-  Serial.print(" Y: "); Serial.print(Y);
-  Serial.print(" Buttons: "); Serial.println(buttons);
+  // 디버깅 모니터 출력
+  Serial.print("X: "); Serial.print(rawX);
+  Serial.print("  Y: "); Serial.print(rawY);
+  Serial.print("  Buttons: "); 
+  Serial.println(tempBtn);  // 이제 '\0'이 있으므로 안전하게 출력됨
 
   delay(50);
 }
